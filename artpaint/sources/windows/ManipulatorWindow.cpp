efc391cbd23a250838cbf6f75ea19c99c6110a3e
julun <julun@152b581b-4430-0410-be25-fb34eca01ab1>
2009-09-02 21:50:10 +0000

    * Cleanup and make use of the layout system.
    git-svn-id: http://svn.osdrawer.net/artpaint@254 152b581b-4430-0410-be25-fb34eca01ab1

--- artpaint/sources/windows/ManipulatorWindow.cpp
+++ artpaint/sources/windows/ManipulatorWindow.cpp
@@ -1,9 +1,11 @@
 /*
  * Copyright 2003, Heikki Suhonen
+ * Copyright 2009, Karsten Heimrich
  * Distributed under the terms of the MIT License.
  *
  * Authors:
  * 		Heikki Suhonen <heikki.suhonen@gmail.com>
+ * 		Karsten Heimrich <host.haiku@gmx.de>
  *
  */
 
@@ -17,17 +19,21 @@
 
 
 #include <Button.h>
+#include <GroupLayout.h>
+#include <GroupLayoutBuilder.h>
+#include <SeparatorView.h>
 
 
-BList* ManipulatorWindow::window_list = new BList();
-sem_id ManipulatorWindow::list_mutex = create_sem(1, "list_mutex");
+BList ManipulatorWindow::sfWindowList(10);
+sem_id ManipulatorWindow::sfWindowListMutex = create_sem(1, "list_mutex");
 
 
-ManipulatorWindow::ManipulatorWindow(BRect rect, BView *view, const char *name,
-		BWindow *master, BMessenger *t)
-	: BWindow(rect ,name, B_TITLED_WINDOW_LOOK, B_NORMAL_WINDOW_FEEL,
-		B_NOT_RESIZABLE | B_NOT_ZOOMABLE | B_NOT_ANCHORED_ON_ACTIVATE
-		| B_NOT_CLOSABLE)
+ManipulatorWindow::ManipulatorWindow(BRect rect, BView* view, const char* name,
+		BWindow* master, BMessenger* target)
+	: BWindow(rect, name, B_TITLED_WINDOW_LOOK, B_NORMAL_WINDOW_FEEL,
+		B_NOT_ZOOMABLE | B_NOT_ANCHORED_ON_ACTIVATE | B_NOT_CLOSABLE
+		| B_AUTO_UPDATE_SIZE_LIMITS)
+	, fManipulatorView(view)
 {
 	window_feel feel = B_NORMAL_WINDOW_FEEL;
 	if (SettingsServer* server = SettingsServer::Instance()) {
@@ -35,64 +41,51 @@ ManipulatorWindow::ManipulatorWindow(BRect rect, BView *view, const char *name,
 		server->GetApplicationSettings(&settings);
 		settings.FindInt32(skAddOnWindowFeel, (int32*)&feel);
 	}
+	SetFeel(feel);
 
 	window_look look = B_FLOATING_WINDOW_LOOK;
 	if (feel == B_NORMAL_WINDOW_FEEL)
 		look = B_TITLED_WINDOW_LOOK;
-
-	SetFeel(feel);
 	SetLook(look);
 
 	AddToSubset(master);
 
-	manipulator_view = view;
+	acquire_sem(sfWindowListMutex);
+	sfWindowList.AddItem(this);
+	release_sem(sfWindowListMutex);
 
-	acquire_sem(list_mutex);
-	window_list->AddItem(this);
-	release_sem(list_mutex);
+	// TODO: remove this once BSeparatorView works properly
+	BSeparatorView* separator = new BSeparatorView(B_HORIZONTAL, B_FANCY_BORDER);
+	separator->SetViewColor(ui_color(B_PANEL_BACKGROUND_COLOR));
 
-	// Here create the background-view
-	ManipulatorWindowBackgroundView* bg_view = new ManipulatorWindowBackgroundView(view->Bounds());
-	bg_view->SetViewColor(ui_color(B_PANEL_BACKGROUND_COLOR));
+	BMessage* okMessage = new BMessage(HS_MANIPULATOR_FINISHED);
+	okMessage->AddBool("status", true);
+	BButton* okButton = new BButton(StringServer::ReturnString(OK_STRING),
+		okMessage);
 
-	// Create the ok and cancel-buttons
-	BButton *ok_button;
-	BButton *cancel_button;
+	BMessage* cancelMessage = new BMessage(HS_MANIPULATOR_FINISHED);
+	cancelMessage->AddBool("status", true);
+	BButton* cancelButton =
+		new BButton(StringServer::ReturnString(CANCEL_STRING), cancelMessage);
 
-	BMessage *ok_message = new BMessage(HS_MANIPULATOR_FINISHED);
-	ok_message->AddBool("status",TRUE);
-	ok_button = new BButton(BRect(0,0,0,0),"ok_button",StringServer::ReturnString(OK_STRING),ok_message);
-	ok_button->MakeDefault(true);
-	ok_button->ResizeToPreferred();
+	SetLayout(new BGroupLayout(B_HORIZONTAL));
+	AddChild(BGroupLayoutBuilder(B_VERTICAL, 10.0)
+		.Add(fManipulatorView)
+		.Add(separator)
+		.AddGroup(B_HORIZONTAL, 10.0)
+			.AddGlue()
+			.Add(okButton)
+			.Add(cancelButton)
+		.End()
+		.SetInsets(10.0, 10.0, 10.0, 10.0)
+	);
 
-
-	BMessage *cancel_message = new BMessage(HS_MANIPULATOR_FINISHED);
-	cancel_message->AddBool("status",FALSE);
-	cancel_button = new BButton(BRect(0,0,0,0),"cancel_button",StringServer::ReturnString(CANCEL_STRING),cancel_message);
-	cancel_button->ResizeToPreferred();
-
-
-	// Resize the background-view
-	bg_view->ResizeBy(0,ok_button->Frame().Height()+21);
-	bg_view->ResizeTo(max_c(2*ok_button->Frame().Width()+100,view->Frame().Width()),bg_view->Frame().Height());
-
-	// Position the buttons and the view
-	ok_button->MoveTo(bg_view->Bounds().right - ok_button->Frame().Width() - 10,bg_view->Bounds().bottom-ok_button->Frame().Height()-10);
-	cancel_button->MoveTo(ok_button->Frame().left-cancel_button->Frame().Width()-10,ok_button->Frame().top+(ok_button->Frame().bottom - ok_button->Frame().top)/2-cancel_button->Frame().Height()/2);
-	view->MoveTo((bg_view->Bounds().Width()-view->Bounds().Width())/2,0);
-
-	ResizeTo(bg_view->Bounds().Width(),bg_view->Bounds().Height());
-	bg_view->MoveTo(0,0);
-	bg_view->AddChild(view);
-	bg_view->AddChild(ok_button);
-	bg_view->AddChild(cancel_button);
-
-	ok_button->SetTarget(*t);
-	cancel_button->SetTarget(*t);
-
-	AddChild(bg_view);
+	okButton->MakeDefault(true);
+	okButton->SetTarget(*target);
+	cancelButton->SetTarget(*target);
 
 	ColorPaletteWindow::AddMasterWindow(this);
+
 	Show();
 	Lock();
 	AddCommonFilter(new BMessageFilter(B_KEY_DOWN,AppKeyFilterFunction));
@@ -103,10 +96,12 @@ ManipulatorWindow::ManipulatorWindow(BRect rect, BView *view, const char *name,
 
 ManipulatorWindow::~ManipulatorWindow()
 {
-	acquire_sem(list_mutex);
-	window_list->RemoveItem(this);
-	release_sem(list_mutex);
-	manipulator_view->RemoveSelf();	// The manipulator will delete this.
+	acquire_sem(sfWindowListMutex);
+	sfWindowList.RemoveItem(this);
+	release_sem(sfWindowListMutex);
+
+	// The manipulator will delete this.
+	fManipulatorView->RemoveSelf();
 
 	if (SettingsServer* server = SettingsServer::Instance()) {
 		server->SetValue(SettingsServer::Application, skAddOnWindowFrame,
@@ -118,7 +113,8 @@ ManipulatorWindow::~ManipulatorWindow()
 
 
 
-void ManipulatorWindow::setFeel(window_feel feel)
+void
+ManipulatorWindow::setFeel(window_feel feel)
 {
 	if (SettingsServer* server = SettingsServer::Instance()) {
 		server->SetValue(SettingsServer::Application, skAddOnWindowFeel,
@@ -129,13 +125,13 @@ void ManipulatorWindow::setFeel(window_feel feel)
 	if (feel == B_NORMAL_WINDOW_FEEL)
 		look = B_TITLED_WINDOW_LOOK;
 
-	acquire_sem(list_mutex);
+	acquire_sem(sfWindowListMutex);
 
-	for (int32 i = 0; i < window_list->CountItems(); ++i) {
-		BWindow *window = static_cast<BWindow*> (window_list->ItemAt(i));
+	for (int32 i = 0; i < sfWindowList.CountItems(); ++i) {
+		BWindow *window = static_cast<BWindow*> (sfWindowList.ItemAt(i));
 		window->SetFeel(feel);
 		window->SetLook(look);
 	}
 
-	release_sem(list_mutex);
+	release_sem(sfWindowListMutex);
 }
--- artpaint/sources/windows/ManipulatorWindow.h
+++ artpaint/sources/windows/ManipulatorWindow.h
@@ -1,9 +1,11 @@
 /*
  * Copyright 2003, Heikki Suhonen
+ * Copyright 2009, Karsten Heimrich
  * Distributed under the terms of the MIT License.
  *
  * Authors:
  * 		Heikki Suhonen <heikki.suhonen@gmail.com>
+ * 		Karsten Heimrich <host.haiku@gmx.de>
  *
  */
 #ifndef MANIPULATOR_WINDOW_H
@@ -11,40 +13,22 @@
 
 #include <Window.h>
 
+
 class ManipulatorWindow : public BWindow {
-static	BList		*window_list;
-static	sem_id		list_mutex;
-
-		BView		*manipulator_view;
-
 public:
-		ManipulatorWindow(BRect,BView*,const char*,BWindow *master,BMessenger*);
-		~ManipulatorWindow();
+								ManipulatorWindow(BRect rect,
+									BView* manipulatorView, const char* name,
+									BWindow* master, BMessenger* target);
+	virtual						~ManipulatorWindow();
 
+	static	void				setFeel(window_feel);
 
+private:
+	static	BList				sfWindowList;
+	static	sem_id				sfWindowListMutex;
 
-void	static	setFeel(window_feel);
+			BView*				fManipulatorView;
 };
 
-
-
-class ManipulatorWindowBackgroundView : public BView {
-public:
-		ManipulatorWindowBackgroundView(BRect rect)
-			: BView(rect,"background-view",B_FOLLOW_ALL_SIDES,B_WILL_DRAW) {}
-
-
-void	Draw(BRect r) {
-			BView::Draw(r);
-			BView *view = FindView("ok_button");
-			if (view != NULL) {
-				float top = view->Frame().top - 10;
-				SetHighColor(tint_color(ui_color(B_PANEL_BACKGROUND_COLOR),B_DARKEN_4_TINT));
-				StrokeLine(BPoint(0,top),BPoint(Bounds().right,top));
-				SetHighColor(tint_color(ui_color(B_PANEL_BACKGROUND_COLOR),B_LIGHTEN_2_TINT));
-				StrokeLine(BPoint(0,top+1),BPoint(Bounds().right,top+1));
-			}
-		}
-};
-#endif
+#endif	// MANIPULATOR_WINDOW_H
 
