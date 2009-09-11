c22a9fb6916fbe0c1d136cfc3a9aeea7cd967ebd
julun <julun@152b581b-4430-0410-be25-fb34eca01ab1>
2009-09-11 20:24:56 +0000

    * remove some more dead stuff
    git-svn-id: http://svn.osdrawer.net/artpaint@293 152b581b-4430-0410-be25-fb34eca01ab1

--- artpaint/sources/application/ClipBoardManager.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2003, Heikki Suhonen
- * Distributed under the terms of the MIT License.
- *
- * Authors:
- * 		Heikki Suhonen <heikki.suhonen@gmail.com>
- *
- */
-#include "ClipBoardManager.h"
