2cfa0b79e0779629960920c420f7d8a766388cdb
Pete Goodeve <pete.goodeve@computer.org>
2017-03-30 17:56:51 -0700

    Fixed up add-ons, and added documentation back.

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ echo "Building ArtPaint"
 make -f Makefile OBJ_DIR="objects_artpaint" $*
 echo "Building ArtPaint addons"
 make -f Makefile_addons OBJ_DIR="objects_addons" $*
-echo "Moving final executable and addons into dist folder"
+echo "Moving final executable, addons, and Documentation into dist folder"
 mkdir -p dist/add-ons
 cp -af objects_artpaint/ArtPaint dist
 cp -rf addons/AddOns/Working/*/objects_addons/*.so dist/add-ons
+cp -arf Documentation dist/Documentation
 echo "Build has finished!"
