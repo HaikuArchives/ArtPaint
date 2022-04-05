8f68bde8649074412d4eb50781773850e183abe1
Dale Cieslak <dcieslak@yahoo.com>
2022-04-05 04:26:11 -0700

    Localization: fixed errors in de.catkeys and added bindcatalog to build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 echo "Building ArtPaint"
 make -f Makefile OBJ_DIR="objects_artpaint" $*
+make -f Makefile OBJ_DIR="objects_artpaint" bindcatalogs
 echo "Building ArtPaint addons"
 make -f Makefile_addons OBJ_DIR="objects_addons" $*
 echo "Moving final executable, addons, and Documentation into dist folder"
