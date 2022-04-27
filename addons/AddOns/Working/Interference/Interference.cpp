74a4ef68c6c841fc74bf443a3179c52411b929ae
Dale Cieslak <dcieslak@yahoo.com>
2022-04-27 10:55:17 -0700

    Localization: get name and help strings from add-ons for menu (#147)
    To enable translation, briefly instantiate each add-on to get its
    name and help string back via the accessor methods. This allows
    them to be translated via an add-on's own translation context.
    
    Related to #146

--- addons/AddOns/Working/Interference/Interference.cpp
+++ addons/AddOns/Working/Interference/Interference.cpp
@@ -50,6 +50,7 @@ InterferenceManipulator::InterferenceManipulator(BBitmap *bm)
 	livePreview = false;
 	copy_of_the_preview_bitmap = NULL;
 	preview_bitmap = NULL;
+	config_view = NULL;
 
 	SetPreviewBitmap(bm);
 
