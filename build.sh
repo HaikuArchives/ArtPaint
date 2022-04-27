517c79678292e208ba61c9247070b189db019cdc
Humdinger <humdingerb@gmail.com>
2022-04-27 19:42:31 +0200

    Move Add-ons out of "Working" folder
    * Adjust Makefiles
    * Remove "Working" folder
    * Change "build.sh" to invoke the addons' Makefiles in a loop

--- build.sh
+++ build.sh
@@ -2,11 +2,19 @@
 echo "Building ArtPaint"
 make -f Makefile OBJ_DIR="objects_artpaint" $*
 make -f Makefile OBJ_DIR="objects_artpaint" bindcatalogs
+
 echo "Building ArtPaint addons"
-make -f Makefile_addons OBJ_DIR="objects_addons" $*
+for addonDir in $(find ./addons/AddOns/* -maxdepth 0 -type d) ; do
+	pushd "$addonDir"
+	make OBJ_DIR="objects_addons" $*
+	make OBJ_DIR="objects_addons" bindcatalogs
+	popd
+done
+
 echo "Moving final executable, addons, and Documentation into dist folder"
 mkdir -p dist/add-ons
 cp -af objects_artpaint/ArtPaint dist
-cp -rf addons/AddOns/Working/*/objects_addons/*.so dist/add-ons
+cp -rf addons/AddOns/*/objects_addons/*.so dist/add-ons
 cp -arf Documentation dist/Documentation
+
 echo "Build has finished!"
